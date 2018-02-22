00534d54cd94732d6b021da77e0c6d2343fb77a5
Matheus Schettino <schetitno2@gmail.com>
2018-02-22 11:11:49 -0300

    store integration

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import createHistory from 'history/createBrowserHistory';
+import {ConnectedRouter} from 'react-router-redux';
+import {Provider} from 'react-redux';
+
 import './styles/globals.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
+import configureStore from './configureStore';
 
 const MOUNT_NODE = document.getElementById('root');
 
+const initialState = {};
+const history = createHistory();
+const store = configureStore(initialState, history);
+
 function render() {
-  ReactDOM.render(<App />, MOUNT_NODE);
+  ReactDOM.render(
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <App />
+      </ConnectedRouter>
+    </Provider>,
+    MOUNT_NODE,
+  );
 }
 
 render();
