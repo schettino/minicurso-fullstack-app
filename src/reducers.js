b0e93914f3e02fe75fed460cea37ae25648ef0d7
Matheus Schettino <schetitno2@gmail.com>
2018-02-22 11:11:22 -0300

    configureStore and reducers based on React-Boilerplate ✨

---
+++ src/reducers.js
@@ -0,0 +1,34 @@
+/**
+ * Combine all reducers in this file and export the combined reducers.
+ */
+
+import {combineReducers} from 'redux';
+import {LOCATION_CHANGE} from 'react-router-redux';
+
+/*
+ * routeReducer
+ *
+ * The reducer merges route location changes into our state.
+ * The change is necessitated by moving to react-router-redux@4
+ *
+ */
+
+function location(state = null, action) {
+  switch (action.type) {
+    case LOCATION_CHANGE:
+      return action.payload;
+    default:
+      return state;
+  }
+}
+const routeReducer = combineReducers({location});
+
+/**
+ * Creates the main reducer with the dynamically injected ones
+ */
+export default function createReducer(injectedReducers) {
+  return combineReducers({
+    route: routeReducer,
+    ...injectedReducers,
+  });
+}
