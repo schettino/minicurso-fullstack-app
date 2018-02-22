8b0bf0157ed0a393b8a3aea1cf89b6f189ef14c9
Matheus Schettino <schetitno2@gmail.com>
2018-02-22 17:54:53 -0300

    filter actions

--- src/containers/HomePage/actions.js
+++ src/containers/HomePage/actions.js
@@ -1,5 +1,7 @@
 import * as types from './constants';
 
+/**  Load Coin List */
+
 export function loadCoinlist() {
   return {type: types.LOAD_COINLIST};
 }
@@ -10,4 +12,14 @@ export function loadCoinlisttOk(coinlist) {
 
 export function loadCoinlistError(error) {
   return {type: types.LOAD_COINLIST_ERROR, error};
+}
+
+/** Filter Coins */
+
+export function filterCoins(searchTerm) {
+  return {type: types.FILTER_COINS, payload: {searchTerm}};
+}
+
+export function filterCoinsOk(result) {
+  return {type: types.FILTER_COINS_OK, payload: {result}};
 }
--- src/containers/HomePage/constants.js
+++ src/containers/HomePage/constants.js
@@ -1,3 +1,7 @@
 export const LOAD_COINLIST = 'app/HomePage/LOAD_COINLIST';
 export const LOAD_COINLIST_OK = 'app/HomePage/LOAD_COINLIST_OK';
 export const LOAD_COINLIST_ERROR = 'app/HomePage/LOAD_COINLIST_ERROR';
+
+export const FILTER_COINS = 'app/HomePage/FILTER_COINS';
+export const FILTER_COINS_OK = 'app/HomePage/FILTER_COINS_OK';
+export const FILTER_COINS_ERROR = 'app/HomePage/FILTER_COINS_ERROR';
