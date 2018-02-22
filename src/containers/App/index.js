bebc30a44ff04ead8db7265e98b946fbd537bc0a
Matheus Schettino <schetitno2@gmail.com>
2018-02-22 13:14:28 -0300

    container on app index

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom';
+import {Segment, Container} from 'semantic-ui-react';
+// import logo from '../../assets/logo.svg';
 
 import HomePage from '../HomePage';
 import NotFoundPage from '../NotFoundPage';
 
 class App extends React.PureComponent {
+  handleItemClick = () => {};
   render() {
     return (
-      <div className="App">
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route component={NotFoundPage} />
-        </Switch>
+      <div>
+        <Segment basic padded as={Container}>
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+            <Route component={NotFoundPage} />
+          </Switch>
+        </Segment>
       </div>
     );
   }
