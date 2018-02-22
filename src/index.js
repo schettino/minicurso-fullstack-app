07aa2ba7e261061bd6f83eca0e0662e8fcefb7f7
Matheus Schettino <schetitno2@gmail.com>
2018-02-22 10:49:47 -0300

    hot reloading

--- src/index.js
+++ src/index.js
@@ -4,5 +4,21 @@ import './styles/globals.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const MOUNT_NODE = document.getElementById('root');
+
+function render() {
+  ReactDOM.render(<App />, MOUNT_NODE);
+}
+
+render();
 registerServiceWorker();
+
+if (module.hot) {
+  // Hot reloadable React components and translation json files
+  // modules.hot.accept does not accept dynamic dependencies,
+  // have to be constants at compile-time
+  module.hot.accept(['./containers/App'], () => {
+    ReactDOM.unmountComponentAtNode(MOUNT_NODE);
+    render();
+  });
+}
