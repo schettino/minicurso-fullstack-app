c9faeb595f7d2100a7d8e0d9873be52e42fdada0
Matheus Schettino <schetitno2@gmail.com>
2018-02-22 17:55:33 -0300

    saga for filtering coins

--- src/containers/HomePage/saga.js
+++ src/containers/HomePage/saga.js
@@ -1,4 +1,4 @@
-import {all, call, takeLatest, put} from 'redux-saga/effects';
+import {all, call, takeLatest, put, throttle, select} from 'redux-saga/effects';
 import * as types from './constants';
 import * as actions from './actions';
 import request from '../../utils/request';
@@ -12,6 +12,27 @@ export function* loadCoinlist(action) {
   }
 }
 
+function* filterCoinsSaga(action) {
+  const {searchTerm} = action.payload;
+  if (!searchTerm || searchTerm.length === 0) {
+    yield put(actions.filterCoinsOk([]));
+    return;
+  }
+  const coinlist = yield select(state => state.home.coinlist);
+  const searchlist = [];
+  for (let i = 0; i < coinlist.length; ++i) {
+    const {coinName} = coinlist[i];
+    if (coinName && coinName.toLowerCase().includes(searchTerm)) {
+      searchlist.push(coinlist[i]);
+    }
+    if (searchlist.length >= 6) break;
+  }
+  yield put(actions.filterCoinsOk(searchlist));
+}
+
 export default function* saga() {
-  yield all([takeLatest(types.LOAD_COINLIST, loadCoinlist)]);
+  yield all([
+    takeLatest(types.LOAD_COINLIST, loadCoinlist),
+    throttle(450, types.FILTER_COINS, filterCoinsSaga),
+  ]);
 }
