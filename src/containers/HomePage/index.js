4bc6639f6b07892bbd680ce79ac55feb24f444d5
Matheus Schettino <schetitno2@gmail.com>
2018-02-22 17:17:22 -0300

    render initial list

--- src/containers/HomePage/index.js
+++ src/containers/HomePage/index.js
@@ -1,37 +1,85 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
-import {Segment, Input, Transition} from 'semantic-ui-react';
+import {Segment, Input, Transition, Image, Header} from 'semantic-ui-react';
+import {loadCoinlist} from './actions';
 
 class HomePage extends React.Component {
-  componentDidMount() {
-    setTimeout(() => this.searchField.focus(), 500);
+  componentWillMount() {
+    this.props.dispatch(loadCoinlist());
   }
+
+  componentDidMount() {
+    setTimeout(() => this.searchField.focus(), 750);
+  }
+
+  renderCoins = () => {
+    const {coinlist, searchlist} = this.props;
+    if (!coinlist.length) return;
+
+    let items = searchlist;
+    if (!searchlist.length) {
+      const start = Math.round(Math.random() * coinlist.length - 6);
+      items = coinlist.slice(start, start + 6);
+    }
+
+    return (
+      <div>
+        {items.map((item, index) => (
+          <Transition
+            key={item.id}
+            animation="fade down"
+            duration={250 * index}
+            transitionOnMount>
+            <Segment size="tiny" padded>
+              <Header as="h2">
+                <Image inline size="tiny" circular src={item.imageUrl} />
+                <Header.Content>
+                  {item.coinName}
+                  <Header.Subheader content={item.name} />
+                </Header.Content>
+              </Header>
+            </Segment>
+          </Transition>
+        ))}
+      </div>
+    );
+  };
 
   render() {
     return (
-      <Transition animation="slide down" duration={500} transitionOnMount>
-        <Segment floated color="teal" raised size="massive">
-          <Input
-            ref={searchField => {
-              this.searchField = searchField;
-            }}
-            icon="btc"
-            transparent
-            iconPosition="left"
-            fluid
-            size="huge"
-            placeholder="Search for a crypto currency"
-          />
-        </Segment>
-      </Transition>
+      <div>
+        <Transition animation="slide down" duration={500} transitionOnMount>
+          <Segment color="teal" raised size="massive">
+            <Input
+              ref={searchField => {
+                this.searchField = searchField;
+              }}
+              icon="btc"
+              transparent
+              iconPosition="left"
+              fluid
+              size="huge"
+              placeholder="Search for a crypto currency"
+            />
+          </Segment>
+        </Transition>
+        <div>{this.renderCoins()}</div>
+      </div>
     );
   }
 }
 
 function mapStateToProps(state) {
   return {
-    number: state.home.number,
+    coinlist: state.home.coinlist,
+    searchlist: state.home.searchlist,
   };
 }
+
+HomePage.propTypes = {
+  coinlist: PropTypes.array.isRequired,
+  searchlist: PropTypes.array.isRequired,
+};
 
 export default connect(mapStateToProps)(HomePage);
